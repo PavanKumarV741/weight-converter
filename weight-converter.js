16383e43e99bd5288f3f9732576fe8f5e0065d69
PavanKumarV741 <102721175+PavanKumarV741@users.noreply.github.com>
2023-02-28 21:42:06 +0530

    coverting kg to g,pounds,ounces

---
+++ weight-converter.js
@@ -0,0 +1,17 @@
+// var pounds=document.getElementById("pounds")
+// var grams=document.getElementById("grams")
+// var kilograms=document.getElementById("kilograms")
+// var ounces=document.getElementById("ounces")
+// var demo=document.getElementById("demo")
+
+// function convert(){
+//     if(pounds.value==""){
+//     demo.innerText="please enter a valid input"
+
+// }else{
+//     grams.innerHTML=(pounds.value*453.6).toFixed(2)+" g"
+//     kilograms.innerHTML=(pounds.value*0.45).toFixed(2)+" Kg"
+//     ounces.innerHTML=(pounds.value*16).toFixed(2)+" O"
+//     demo.innerText=""
+// }
+// }
